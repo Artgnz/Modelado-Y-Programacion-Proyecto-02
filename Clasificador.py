bf3ee5138250e35eb352ac419c60607601e45ee8
Arsenio <arsenio@ciencias.unam.mx>
2022-11-17 21:34:21 -0600

    Creacion de metodos obtenerVertices AUXILIARES

--- Clasificador.py
+++ Clasificador.py
@@ -1,4 +1,5 @@
 import math
+import statistics
 
 def obtenerDistancias(centros, contornos):
     distancias = {}
@@ -13,4 +14,38 @@ def obtenerDistancias(centros, contornos):
 def calcularDistancia(centro, contorno):
     return math.sqrt(((contorno[0]-centro[0])**2) + ((contorno[1]-centro[1])**2))
 
-def
+def suavizarDistancias(distancias):
+    distanciasSuavizadas = {}
+    for color in distancias:
+        lista1 = []
+        lista2 = distancias[color]
+        for i in distancias:
+            lista1.push(statistics.mean(lista1[i-2:i+3]))
+        distanciasSuavizadas[color] = lista1
+    return distanciasSuavizadas
+
+def obtenerVertices(distancias):
+    vertices = {}
+    for color in distancias:
+        peakIndices = []
+        peakIndex = None
+        peakValue = None
+        signal = distancias[color]
+        baseline = statistics.mean(signal)
+
+        for i, value in signal:
+            if value > baseline:
+                if peakValuea == None or value > peakValue:
+                    peakValue = value
+                    peakIndex = i
+                else:
+                    peakIndices.push(peakIndex)
+                    peakIndex = None
+                    peakValue = None
+
+        if peakIndex != None:
+            peakIndices.push(peakIndex)
+
+        vertices[color] = peakIndices
+
+    return vertices    
